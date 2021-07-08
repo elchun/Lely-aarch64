b7cd3073a4073621aa10ee6a81495af1032ba2d4
elchun <74155909+elchun@users.noreply.github.com>
2021-07-08 17:43:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 cp -r usr/local/bin/* /usr/local/bin
 cp -r usr/local/etc/* /usr/local/etc
 cp -r usr/local/include/* /usr/local/include
 cp -r usr/local/lib/* /usr/local/lib
 
+echo "Lely installed successfully"
